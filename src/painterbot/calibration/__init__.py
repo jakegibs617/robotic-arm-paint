87d14160439d4a34a32c954168c9a2912dcda423
Jake Giberson <jakegibs617@gmail.com>
2026-07-04 11:36:04 -0400

    Refactor calibration helpers for headless tests

--- src/painterbot/calibration/__init__.py
+++ src/painterbot/calibration/__init__.py
@@ -6,8 +6,11 @@ four-corner workflow that maps image pixels to paper coordinates.
 """
 
 from painterbot.calibration.homography import (
+    PAPER_CORNER_ORDER,
     compute_homography,
     image_to_paper,
+    paper_corners_mm,
+    validate_image_corners,
 )
 from painterbot.calibration.pose_calibration import (
     REQUIRED_POSES,
@@ -18,9 +21,12 @@ from painterbot.calibration.pose_calibration import (
 
 __all__ = [
     "CalibrationSession",
+    "PAPER_CORNER_ORDER",
     "REQUIRED_POSES",
     "compute_homography",
     "image_to_paper",
     "is_calibrated",
     "missing_poses",
+    "paper_corners_mm",
+    "validate_image_corners",
 ]
